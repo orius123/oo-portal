f3e0a956380f5ef3f4326aee967f8dcae4867dbf
Matan Zruya <mzruya@gmail.com>
2014-07-06 13:19:26 +0300

    gulp support + .gitignore file

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var bower = require('gulp-bower');
+var vulcanize = require('gulp-vulcanize');
+
+gulp.task('default', function () {
+    return gulp.src('src/index.html')
+        .pipe(vulcanize({dest: 'dist'}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest('lib/'))
+});
