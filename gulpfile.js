f2ab392746c248c6d1752b166cc77433e7669223
mzruya <mzruya@gmail.com>
2014-07-06 14:51:31 +0300

    live reload support

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
 var vulcanize = require('gulp-vulcanize');
+var connect = require('gulp-connect');
 
-gulp.task('default', function () {
+gulp.task('vulcanize', function () {
     return gulp.src('src/index.html')
-        .pipe(vulcanize({dest: 'dist', inline : true}))
+        .pipe(vulcanize({dest: 'dist', inline: true}))
         .pipe(gulp.dest('dist'));
 });
 
@@ -12,3 +13,27 @@ gulp.task('bower', function () {
     return bower()
         .pipe(gulp.dest('lib/'));
 });
+
+gulp.task('connect', function () {
+    connect.server({
+        livereload: true
+    });
+});
+
+gulp.task('html', function () {
+    gulp.src('./src/*.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('json', function () {
+    gulp.src('./mock-server/*.json')
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./src/*.html'], ['html']);
+    gulp.watch(['./mock-server/*.json'], ['json']);
+});
+
+gulp.task('server', ['connect', 'watch']);
+gulp.task('default', ['vulcanize']);
